6de047804e3500c2d97a35ffe705b625f592eafa
Luca <luke.pisa@gmail.com>
2023-05-08 16:10:32 +0200

    inizializzo classe astratta Veicolo

--- Inventario_veicoli/src/Package_inventario/Veicolo.java
+++ Inventario_veicoli/src/Package_inventario/Veicolo.java
@@ -1,5 +1,41 @@
 package Package_inventario;
 
 public abstract class Veicolo {
+	
+	private String marca;
+	private String targa;
+	private String modello;
+	
+	// getters e setters
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getTarga() {
+		return targa;
+	}
+	public void setTarga(String targa) {
+		this.targa = targa;
+	}
+	public String getModello() {
+		return modello;
+	}
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+	
+	// costruttore non istanziabile, risparmio codice nelle figlie
+	public Veicolo(String marca, String targa, String modello) {
+		this.marca = marca;
+		this.targa = targa;
+		this.modello = modello;
+	}
+	
+	
+	
+	
+	
 
 }
