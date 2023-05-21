b56db186b2891458d7d16852c840af10d62f26d2
Fed321 <fede2141@gmail.com>
2023-05-21 16:22:49 +0200

    Aggiunto il javaDoc nella classe Moto

--- Inventario_veicoli/src/Package_inventario/Moto.java
+++ Inventario_veicoli/src/Package_inventario/Moto.java
@@ -1,22 +1,45 @@
 package Package_inventario;
 
+/**
+ * Classe che rappresenta l'oggetto Moto
+ * @author ChurPeezZoli
+ * @since 04/2023
+ */
 public class Moto extends Veicolo {
 
 	private int cilindrata;
 	
+	/**
+	 * Costruttore della classe Moto
+	 * @param marca
+	 * @param targa
+	 * @param modello
+	 * @param cilindrata
+	 */
 	public Moto(String marca, String targa, String modello, int cilindrata) {
 		super(marca, targa, modello);
 		this.cilindrata = cilindrata;
 	}
 
+	/**
+	 * metodo che ritorna la cilindrata della moto
+	 * @return
+	 */
 	public int getCilindrata() {
 		return cilindrata;
 	}
 
+	/**
+	 * metodo che permette di settare la cilindrata della moto
+	 * @param cilindrata
+	 */
 	public void setCilindrata(int cilindrata) {
 		this.cilindrata = cilindrata;
 	}
 	
+	/**
+	 * metodo che ritorna la stringa che esplicita le proprietà dell'oggetto Moto
+	 */
 	public String toString() {
 		return "Moto con cilindrata di " + this.cilindrata + " cc. " + super.toString();
 	}
